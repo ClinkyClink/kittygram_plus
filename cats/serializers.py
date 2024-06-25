b6e2b3811fe9453a19bda4f3bf715e78bdefc7f1
Евгений Аркатов <thejekaarkatov@yandex.ru>
2024-06-25 21:05:06 +0300

    fd

--- cats/serializers.py
+++ cats/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 import webcolors
 
-from .models import Cat, Owner, Achievement, AchievementCat
+from .models import Cat, Owner, Achievement, AchievementCat, CHOICES
 
 
 class Hex2NameColor(serializers.Field):
@@ -65,6 +65,14 @@ class CatSerializer(serializers.ModelSerializer):
             AchievementCat.objects.create(
                 achievement=current_achievement, cat=cat)
         return cat 
+
+
+class CatListSerializer(serializers.ModelSerializer):
+    color = serializers.ChoiceField(choices=CHOICES)
+    
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color') 
 
 
 class OwnerSerializer(serializers.ModelSerializer):
--- cats/views.py
+++ cats/views.py
@@ -3,12 +3,26 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Cat, Owner
-from .serializers import CatSerializer, OwnerSerializer
+from .serializers import CatSerializer, OwnerSerializer, CatListSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+
+    @action(detail=False, url_path='recent-white-cats')
+    def recent_white_cats(self, request):
+        cats = Cat.objects.filter(color='Whire')[:5]
+        serializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data)
+
+    def get_serializer_class(self):
+        # Если запрошенное действие (action) — получение списка объектов ('list')
+        if self.action == 'list':
+            # ...то применяем CatListSerializer
+            return CatListSerializer
+        # А если запрошенное действие — не 'list', применяем CatSerializer
+        return CatSerializer 
 
 
 class OwnerViewSet(viewsets.ModelViewSet):
